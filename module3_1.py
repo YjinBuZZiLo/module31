0aca091875ae0fe86be017c739143672c8451478
YjinBuZZiLo <evgene2305@gmail.com>
2024-08-18 13:00:20 +0300

    Add files via upload

---
+++ module3_1.py
@@ -0,0 +1,25 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.upper()
+    list_to_search = [item.upper() for item in list_to_search]
+    return string in list_to_search
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))    # No matches
+print(calls)
